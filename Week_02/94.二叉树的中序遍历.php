dc61fb2cedb2a7793b95f52bf2e937d8f8feb178
sky930901 <1319891822@qq.com>
2020-07-05 22:04:29 +0800

    -m第二周作业

---
+++ Week_02/94.二叉树的中序遍历.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=94 lang=php
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[]
+     */
+    function inorderTraversal($root) {
+        $res = [];
+        $this->helper($root,$res);
+        return $res;
+    }
+
+    function helper($root,&$res) {
+
+        if ($root == null) return '';
+        $this->helper($root->left, $res);
+        $res[] = $root->val;
+        $this->helper($root->right,$res);
+    }
+}
+// @lc code=end
+
